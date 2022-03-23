0b4aa492e87eaec01825cf8b526370c5a2a2e589
Henrik Klarup <hkl@siteimprove.com>
2022-03-23 15:46:06 +0100

    Astro template

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	renderers: ['@astrojs/renderer-preact'],
+	buildOptions: {
+		site: 'https://techramblings.dk/',
+	},
+});
